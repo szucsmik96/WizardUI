31089385dd0b4f5e82132d900dc2d794c1262c54
szucsmik96 <szucsmik96@gmail.com>
2018-05-31 20:02:04 +0100

    add alpha version

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+    int max;
+    int min;
+    int guess;
+    int maxGuesses = 12;
+    public Text text;
+    void StartGame()
+    {
+        max = 10000;
+        min = 1;
+        NextGuess();
+    }
+    void NextGuess()
+    {
+        guess = Random.Range(min, max+1);
+        text.text = guess.ToString();
+        maxGuesses--;
+        if (maxGuesses <= 0)
+        {
+            Application.LoadLevel("Win");
+        }
+    }
+    // Use this for initialization
+    void Start () {
+        StartGame();
+	}
+	
+    public void GuessLower(){
+            max = guess;
+            NextGuess();
+    }
+    public void GuessHigher(){
+        min = guess;
+        NextGuess();
+    }
+    public void Correct()
+    {
+        Application.LoadLevel("Lose");
+    }
+}
